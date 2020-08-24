96ff92240d934221609945f8747bc433563fb1b6
Dari Alves <darieltondf@gmail.com>
2020-08-24 16:48:38 -0300

    models, repositories, routes

---
+++ nivel-02/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -0,0 +1,37 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+// SoC: Separation of Concerns (Separações de preocupações)
+
+const appointmentsRouter = Router();
+const appointmentRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  // Salvar a data no formato correto
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentRepository.create(provider, parsedDate);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
--- nivel-02/primeiro-projeto-node/src/server.ts
+++ nivel-02/primeiro-projeto-node/src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import routes from './routes';
 
 const app = express();
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log('🚀 Server Started on port 3000!');
+app.listen(3333, () => {
+  console.log('🚀 Server Started on port 3333!');
 });
