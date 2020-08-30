a393b93d39449ed83d11fdf21ad61336d58381ac
Dari Alves <darieltondf@gmail.com>
2020-08-30 13:45:36 -0300

    gerando token jwt

--- nivel-02/iniciando-backend-app/src/services/AuthenticateUserService.ts
+++ nivel-02/iniciando-backend-app/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/Users';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -33,10 +35,16 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    const token = sign({}, '98325e8dccf3cb81b043b9cdd2821480', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     // Se Passou: Usuário autenticado
 
     return {
       user,
+      token,
     };
   }
 }
