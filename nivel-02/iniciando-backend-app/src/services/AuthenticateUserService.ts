efe626aff38396eae4704fcac1cdef6c83537844
Dari Alves <darieltondf@gmail.com>
2020-08-31 09:57:33 -0300

    criando classe de erro

--- nivel-02/iniciando-backend-app/src/services/AuthenticateUserService.ts
+++ nivel-02/iniciando-backend-app/src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
 
 import User from '../models/User';
 
@@ -22,7 +23,7 @@ class AuthenticateUserService {
     const user = await userRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     /*
@@ -33,7 +34,7 @@ class AuthenticateUserService {
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Incorrect email/password combination.');
+      throw new AppError('Incorrect email/password combination.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
