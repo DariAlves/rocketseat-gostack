4b889de2c87bec664c741e974915c6ff6f6d0ea0
Dari Alves <darieltondf@gmail.com>
2020-09-01 11:06:31 -0300

    estilizando dashboard

--- nivel-03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ nivel-03/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://vignette.wikia.nocookie.net/p__/images/0/00/The-Hobbit-photo-character-Gandalf.jpeg/revision/latest?cb=20190112144346&path-prefix=protagonist"
+            alt="Gandalf"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://vignette.wikia.nocookie.net/p__/images/0/00/The-Hobbit-photo-character-Gandalf.jpeg/revision/latest?cb=20190112144346&path-prefix=protagonist"
+            alt="Gandalf"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://vignette.wikia.nocookie.net/p__/images/0/00/The-Hobbit-photo-character-Gandalf.jpeg/revision/latest?cb=20190112144346&path-prefix=protagonist"
+            alt="Gandalf"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
