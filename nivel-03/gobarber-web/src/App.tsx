18dbfbbc6ffec507f2fcd50a325ecbb396420034
Dari Alves <darieltondf@gmail.com>
2020-09-06 17:02:59 -0300

    login pelo contexto

--- nivel-03/gobarber-web/src/App.tsx
+++ nivel-03/gobarber-web/src/App.tsx
@@ -3,13 +3,13 @@ import React from 'react';
 import SignIn from './pages/Signin';
 import GlobalStyle from './styles/global';
 
-import AuthContext from './context/AuthContext';
+import { AuthProvider } from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <AuthContext.Provider value={{ name: 'Dari' }}>
+    <AuthProvider>
       <SignIn />
-    </AuthContext.Provider>
+    </AuthProvider>
 
     <GlobalStyle />
   </>
