2b12991e0776a817cea8712532b91d6c50105ec0
Dari Alves <darieltondf@gmail.com>
2020-09-11 21:55:17 -0300

    update rotas privadas

--- nivel-03/appgobarber/src/hooks/auth.tsx
+++ nivel-03/appgobarber/src/hooks/auth.tsx
@@ -72,7 +72,7 @@ const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, signIn, signOut, loading }}>
       {children}
     </AuthContext.Provider>
   );
