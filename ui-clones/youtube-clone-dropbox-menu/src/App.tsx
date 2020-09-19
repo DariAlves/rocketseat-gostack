3c21457780629ec9c86dcbae1e7f317a67912a3c
Dari Alves <darieltondf@gmail.com>
2020-09-18 23:09:32 -0300

    ui clones

---
+++ ui-clones/youtube-clone-dropbox-menu/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import GlobalStyles from './styles/GlobalStyles';
+import Section from './components/Section';
+
+function App() {
+  return (
+    <>
+      <Section />
+
+      <GlobalStyles />
+    </>
+  );
+}
+
+export default App;
---
+++ ui-clones/youtube-clone-dropbox-menu/src/components/Section/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+interface Props {
+    variant: 'blue' | 'beige' | 'white' | 'black';
+}
+
+const Section: React.FC = () => {
+    return (
+        <div>Section</div>
+    );
+}
+
+export default Section;
