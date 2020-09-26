0e928914bea25654f92ef2f7af6e46898dd03ac3
Dari Alves <darieltondf@gmail.com>
2020-09-26 13:27:08 -0300

    usestate

--- rocketseat-youtube/githooks/src/App.js
+++ rocketseat-youtube/githooks/src/App.js
@@ -1,48 +1,81 @@
 import React, { useState, useEffect } from 'react';
 
-function App() {
-  const [repositories, setRepositories] = useState([]);
+export default function App() {
+  const [location, setLocation] = useState({})
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('https://api.github.com/users/darialves/repos');
-      const data = await response.json();
-      setRepositories(data);
-    }
+    navigator.geolocation.watchPosition(handlePositionReceived);
 
-    fetchData();
-
+    // return () => navigator.geolocation.clearWatch(watchID);
   }, [])
 
-  // useEffect(async () => {
-  //     const response = await fetch('https://api.github.com/users/darialves/repos');
-  //     const data = await response.json();
-  //     setRepositories(data);
+  function handlePositionReceived({ coords }) {
+    const { latitude, longitude } = coords;
 
-
-  // }, [])
-
-  function handleFavorite(id) {
-    const newRepositories = repositories.map(repository => {
-      return repository.id === id ? { ...repository, favorite: !repository.favorite } : repository
-    });
-
-
-    setRepositories(newRepositories);
+    setLocation({
+      latitude,
+      longitude
+    })
   }
 
-  return (
-        <ul>
-          {repositories.map((repository) => ( 
-            <li key={repository.id}>
-              {repository.name}
-              {repository.favorite && <span>(Favorito)</span>}
-              <button onClick={() => handleFavorite(repository.id)}>Favoritar</button>
-            </li>
-          ))}
-        </ul>
+  return(
+    <>
+      Latitude: {location.latitude} <br />
+      Longitude: {location.longitude}
+    </>
   );
 }
 
-export default App;
+
+// function App() {
+//   const [repositories, setRepositories] = useState([]);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       const response = await fetch('https://api.github.com/users/darialves/repos');
+//       const data = await response.json();
+//       setRepositories(data);
+//     }
+
+//     fetchData();
+
+//   }, []);
+
+//   useEffect(() => {
+//     const filtered = repositories.filter(repo => repo.favorite);
+
+//     document.title = `Você tem ${filtered.length} favoritos`
+//   }, [repositories])
+
+//   // useEffect(async () => {
+//   //     const response = await fetch('https://api.github.com/users/darialves/repos');
+//   //     const data = await response.json();
+//   //     setRepositories(data);
+
+
+//   // }, [])
+
+//   function handleFavorite(id) {
+//     const newRepositories = repositories.map(repository => {
+//       return repository.id === id ? { ...repository, favorite: !repository.favorite } : repository
+//     });
+
+
+//     setRepositories(newRepositories);
+//   }
+
+//   return (
+//         <ul>
+//           {repositories.map((repository) => ( 
+//             <li key={repository.id}>
+//               {repository.name}
+//               {repository.favorite && <span>(Favorito)</span>}
+//               <button onClick={() => handleFavorite(repository.id)}>Favoritar</button>
+//             </li>
+//           ))}
+//         </ul>
+//   );
+// }
+
+// export default App;
 
